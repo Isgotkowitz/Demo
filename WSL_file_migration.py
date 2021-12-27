e3aa513ea3cd0528b59a362ca6f35a5b886a2d19
Ed, XPS Personal <isgotkowitz@gmail.com>
2021-12-26 21:18:59 -0600

    Add code to repo

---
+++ WSL_file_migration.py
@@ -0,0 +1,13 @@
+'''
+Test program to see how to write a program in Atom on Windows then save and run
+it on WSL
+'''
+
+def main():
+    i = 0
+    while i < 10:
+        print(i);
+        i += 1
+
+if __name__ == "__main__":
+    main()
